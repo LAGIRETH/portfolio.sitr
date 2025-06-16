a2d2c800e006fad0614064a2c1d38dcd3bd7b50e
LAGIRETH <lagirethvondrakier@gmail.com>
2025-06-16 20:04:52 +0545

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,1 @@
-// Replace with your actual Supabase values
-const SUPABASE_URL = 'https://wexxbmczibqritujgjud.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndleHhibWN6aWJxcml0dWpnanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNzM2MjUsImV4cCI6MjA2NTY0OTYyNX0.Lk-HQPNI6ywoY09fFT8i5VjyWqr5mIwIXuhSpl9F3X8';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
