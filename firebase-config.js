5dad3f1380e4bd6da4201996b48819f14fdaf272
LAGIRETH <lagirethvondrakier@gmail.com>
2025-06-16 17:38:14 +0545

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// supabase-config.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm
+  apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndleHhibWN6aWJxcml0dWpnanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNzM2MjUsImV4cCI6MjA2NTY0OTYyNX0.Lk-HQPNI6ywoY09fFT8i5VjyWqr5mIwIXuhSpl9F3X8",
+  authDomain: "https://wexxbmczibqritujgjud.supabase.co",
+  projectId: "wexxbmczibqritujgjud",
 };
