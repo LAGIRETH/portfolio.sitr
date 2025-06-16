357cd68693d2c36d29e5cee664b5abe5b94263da
LAGIRETH <lagirethvondrakier@gmail.com>
2025-06-16 18:54:26 +0545

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
+// Replace these with your actual project values
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-const SUPABASE_URL = 'https://wexxbmczibqritujgjud.supabase.co'; // Replace with your Supabase project URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndleHhibWN6aWJxcml0dWpnanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNzM2MjUsImV4cCI6MjA2NTY0OTYyNX0.Lk-HQPNI6ywoY09fFT8i5VjyWqr5mIwIXuhSpl9F3X8'; // Replace with your Supabase anon key
+const SUPABASE_URL = 'https://wexxbmczibqritujgjud.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndleHhibWN6aWJxcml0dWpnanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNzM2MjUsImV4cCI6MjA2NTY0OTYyNX0.Lk-HQPNI6ywoY09fFT8i5VjyWqr5mIwIXuhSpl9F3X8';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
